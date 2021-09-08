89987b29a4cdc0f24f6c296ea16e409d99507648
alex <zhivoderov.aleksej@gmail.com>
2021-09-08 16:35:33 +0300

    добавил тесты контроллера

--- src/test/java/com/example/feigntest/FeigntestApplicationTests.java
+++ src/test/java/com/example/feigntest/FeigntestApplicationTests.java
@@ -1,8 +1,16 @@
 package com.example.feigntest;
 import com.example.feigntest.RequesterService.Requester;
 import com.example.feigntest.clients.Giph;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,8 +18,8 @@ import java.util.List;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FeigntestApplicationTests {
-
 
     @Test
     public void checkGifUrl(){
@@ -37,6 +45,4 @@ class FeigntestApplicationTests {
         });
 
         }
-
-
     }
